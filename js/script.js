21f694e33e9f2bf578b15921d044d65c299d1ca5
cmarc <40406039+crrmarchese@users.noreply.github.com>
2020-12-28 21:02:00 -0500

    Put copy button and result box in wrapper div

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 // Assignment Code (DOM Elements)
+var resultsEl = document.querySelector("#pw-generator-results");
 var pwlengthEl = document.querySelector("#number-range-output");
 var uppercaseEl = document.querySelector("#uppercase-letters");
 var lowercaseEl = document.querySelector("#lowercase-letters");
 var numbersEl = document.querySelector("#numbers");
 var symbolsEl = document.querySelector("#symbols");
 var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
 
+//Object of all the function names used to create random letters/numbers/symbols
+var randomFunc = {
+  upper: getRandomUpperCase,
+  lower: getRandomLowerCase,
+  number: getRandomNumbers,
+  symbol: getRandomSymbols
+}
 
-// Randomize functions
+// Randomize functions for letters/numbers/symbols
 function getRandomUpperCase() {
   var txtUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return txtUpperCase[Math.floor(Math.random() * txtUpperCase.length)];
@@ -20,7 +29,7 @@ function getRandomLowerCase() {
 
 function getRandomNumbers() {
   var txtNumbers = "0123456789";
-  return txtNumbers[Math.floor(Math.random() * txtNumbers.length)]; 
+  return txtNumbers[Math.floor(Math.random() * txtNumbers.length)];
 }
 
 function getRandomSymbols() {
@@ -28,31 +37,42 @@ function getRandomSymbols() {
   return txtSymbols[Math.floor(Math.random() * txtSymbols.length)];
 }
 
-//Get values based on user selections
-function generatePassword() {
+// Get the length and checkbox values based on user selections
+generateBtn.addEventListener("click", function() {
   var pwlengthRange = +pwlengthEl.value;
   var upperCaseOption = uppercaseEl.checked;
   var lowerCaseOption = lowercaseEl.checked;
   var numbersOption = numbersEl.checked;
   var symbolsOption = symbolsEl.checked;
 
-  console.log(pwlengthRange, upperCaseOption, lowerCaseOption, numbersOption, symbolsOption);
+  resultsEl.innerText = generatePassword(upperCaseOption, lowerCaseOption, numbersOption, symbolsOption, pwlengthRange);
+ 
+});
+
+
+// Generate password function
+function generatePassword(upper, lower, number, symbol, pwlengthRange) {
+  // Init pw var
+  var writePassword = "";
+
+  // Count the number of checked boxes
+  var countCheckedBoxes = (upper + lower + number + symbol);
+
+  //Create an array based on the checked values; Braces create key/values in array
+  var checkedItemArr = [upper, lower, number, symbol];
+  console.log("Checked array: ", checkedItemArr);
+  
+  //Return array with key/value pairs
+
+ 
+  // Filter out unchecked types/false values (If not selected we don't want to include it)
+
+  // Loop over length and call generator function for each type
+
+  // Add final pw to the pw var and return
+
+
 }
-
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#pw-generator-results");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
 
 // Custom Range Slider
 
@@ -65,7 +85,7 @@ var sliderNumOutput = document.getElementById("number-range-output");
 //Returns value from slider control into input box
 sliderNumOutput.innerHTML = sliderRangeNum.value;
 
-// Execute JS when a user writes something in <input> field. 
+// Execute JS when a user writes something in <input> field.
 //Write the value from the slider into the <input> value attribute
 sliderRangeNum.oninput = function() {
   sliderNumOutput.value = this.value;
