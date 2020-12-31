622993fbf39f50bfde7f4fa8be5a8d4c76d8b8c4
cmarc <40406039+crrmarchese@users.noreply.github.com>
2020-12-30 21:19:09 -0500

    Replace script.js file with reworked code

--- js/script.js
+++ js/script.js
@@ -1,92 +1,111 @@
 // Assignment Code (DOM Elements)
-var resultsEl = document.querySelector("#pw-generator-results");
-var pwlengthEl = document.querySelector("#number-range-output");
-var uppercaseEl = document.querySelector("#uppercase-letters");
-var lowercaseEl = document.querySelector("#lowercase-letters");
-var numbersEl = document.querySelector("#numbers");
-var symbolsEl = document.querySelector("#symbols");
-var generateBtn = document.querySelector("#generate");
-var copyBtn = document.querySelector("#copy");
+let pwresultsField = document.querySelector("#pw-generator-results");
+let pwlengthInput = document.querySelector("#number-range-output");
+let uppercaseChBox = document.querySelector("#uppercase-letters");
+let lowercaseChBox = document.querySelector("#lowercase-letters");
+let numbersChBox = document.querySelector("#numbers");
+let symbolsChBox = document.querySelector("#symbols");
+let generateBtn = document.querySelector("#generate");
+let copyBtn = document.querySelector("#copy");
 
-//Object of all the function names used to create random letters/numbers/symbols
-var randomFunc = {
-  upper: getRandomUpperCase,
-  lower: getRandomLowerCase,
-  number: getRandomNumbers,
-  symbol: getRandomSymbols
-}
+//Create variables for characters
+let txtUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let txtLowerCase = "abcdefghijklmnopqrstuvwxyz";
+let txtNumbers = "0123456789";
+let txtSymbols = "!#$%&()*+-/<=>?@[]^_{|}~";
 
 // Randomize functions for letters/numbers/symbols
-function getRandomUpperCase() {
-  var txtUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  return txtUpperCase[Math.floor(Math.random() * txtUpperCase.length)];
-}
+// function getRandomUpperCase() {
+//   let txtUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//   return txtUpperCase[Math.floor(Math.random() * txtUpperCase.length)];
+// }
 
-function getRandomLowerCase() {
-  var txtLowerCase = "abcdefghijklmnopqrstuvwxyz";
-  return txtLowerCase[Math.floor(Math.random() * txtLowerCase.length)];
-}
+// function getRandomLowerCase() {
+//   let txtLowerCase = "abcdefghijklmnopqrstuvwxyz";
+//   return txtLowerCase[Math.floor(Math.random() * txtLowerCase.length)];
+// }
 
-function getRandomNumbers() {
-  var txtNumbers = "0123456789";
-  return txtNumbers[Math.floor(Math.random() * txtNumbers.length)];
-}
+// function getRandomNumbers() {
+//   let txtNumbers = "0123456789";
+//   return txtNumbers[Math.floor(Math.random() * txtNumbers.length)];
+// }
 
-function getRandomSymbols() {
-  var txtSymbols = "!#$%&()*+-/<=>?@[]^_{|}~";
-  return txtSymbols[Math.floor(Math.random() * txtSymbols.length)];
-}
-
-// Get the length and checkbox values based on user selections
-generateBtn.addEventListener("click", function() {
-  var pwlengthRange = +pwlengthEl.value;
-  var upperCaseOption = uppercaseEl.checked;
-  var lowerCaseOption = lowercaseEl.checked;
-  var numbersOption = numbersEl.checked;
-  var symbolsOption = symbolsEl.checked;
-
-  resultsEl.innerText = generatePassword(upperCaseOption, lowerCaseOption, numbersOption, symbolsOption, pwlengthRange);
- 
-});
-
-
-// Generate password function
-function generatePassword(upper, lower, number, symbol, pwlengthRange) {
-  // Init pw var
-  var writePassword = "";
-
-  // Count the number of checked boxes
-  var countCheckedBoxes = (upper + lower + number + symbol);
-
-  //Create an array based on the checked values; Braces create key/values in array
-  var checkedItemArr = [upper, lower, number, symbol];
-  console.log("Checked array: ", checkedItemArr);
-  
-  //Return array with key/value pairs
-
- 
-  // Filter out unchecked types/false values (If not selected we don't want to include it)
-
-  // Loop over length and call generator function for each type
-
-  // Add final pw to the pw var and return
-
-
-}
+// function getRandomSymbols() {
+//   let txtSymbols = "!#$%&()*+-/<=>?@[]^_{|}~";
+//   return txtSymbols[Math.floor(Math.random() * txtSymbols.length)];
+// }
 
 // Custom Range Slider
 
 // Range slider control ID
 var sliderRangeNum = document.getElementById("slider-range-customizer");
 
-//Input box ID to return value from slider control
+// Input box ID to return value from slider control
 var sliderNumOutput = document.getElementById("number-range-output");
 
-//Returns value from slider control into input box
+// Returns value from slider control into input box
 sliderNumOutput.innerHTML = sliderRangeNum.value;
 
-// Execute JS when a user writes something in <input> field.
-//Write the value from the slider into the <input> value attribute
+// Return the value from the slider into the <input> value attribute
 sliderRangeNum.oninput = function() {
   sliderNumOutput.value = this.value;
 }
+
+// When the generate button is clicked, check to see which checkboxes are checked
+generateBtn.addEventListener("click", function() {
+
+  // If no checkboxes are selected, return an alert message
+  // Otherwise get the length value and checkbox values that are true by calling
+  // the generatePassword() function
+
+  if(uppercaseChBox.checked === false && lowercaseChBox.checked === false && numbersChBox.checked === false && symbolsChBox.checked === false) {
+    alert("Please select your options using the checkboxes.");
+  } else {
+    generatePassword();
+  }
+});
+
+function generatePassword() {
+  // Set empty password variable
+   let password = "";
+
+  // Set empty variable for a combination of all checked/selected character types
+  let allAvailableChars = "";
+ 
+ // Get the length and checkbox values based on user selections
+  let pwlengthRange = parseInt(pwlengthInput.value);
+  let upperCaseOption = uppercaseChBox.checked;
+  let lowerCaseOption = lowercaseChBox.checked;
+  let numbersOption = numbersChBox.checked;
+  let symbolsOption = symbolsChBox.checked;
+  // console.log(pwlengthRange, upperCaseOption, lowerCaseOption, numbersOption, symbolsOption);
+  if (upperCaseOption === true) {
+    allAvailableChars += txtUpperCase;
+  }
+  if (lowerCaseOption === true) {
+    allAvailableChars += txtLowerCase;
+  }
+  if (numbersOption === true) {
+    allAvailableChars += txtNumbers;
+  }
+  if (symbolsOption === true) {
+    allAvailableChars += txtSymbols;
+  }
+  //  console.log(allAvailableChars);
+  
+};
+
+//Create loop for password characters
+//for(let i = 0; i <= pwlengthRange; i++) {
+
+//}
+
+// Write password to the pw results field
+// function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = pwresultsField;
+  
+//     passwordText.value = password;
+  
+// }
+
